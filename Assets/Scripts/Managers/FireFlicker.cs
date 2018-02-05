ddff985f23a5778020fe602472350321bb615101
Abigail de Joode <abbydiode@gmail.com>
2018-02-05 09:42:53 +0100

    Big torch update

---
+++ Assets/Scripts/Managers/FireFlicker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlicker : MonoBehaviour
+{
+
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 2f;
+    public float flickerSpeed = 0.1f;
+    public float smoothAmount = 0.1f;
+
+    Light fireLight;
+    float startHeight;
+
+    void Awake()
+    {
+        fireLight = GetComponent<Light>();
+        InvokeRepeating("DoFlicker", flickerSpeed, flickerSpeed);
+    }
+
+    void DoFlicker()
+    {
+        fireLight.intensity = Mathf.Lerp(fireLight.intensity, Random.Range(minIntensity, maxIntensity), smoothAmount);
+    }
+
+    void Update()
+    {
+        print(fireLight.intensity);
+    }
+}
---
+++ Assets/Scripts/Player/ButtonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public Vector3 SpawnPosition = Vector3.zero;
+
+    bool gamePaused = false;
+
+    [HideInInspector]
+    public bool debugMode = false;
+
+    FPSController ControllerScript;
+    void Awake()
+    {
+        ControllerScript = GetComponent<FPSController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Reload")) // Reload Player Position
+        {
+            transform.position = SpawnPosition;
+            ControllerScript.verticalVelocity = 0f;
+        }
+
+        if (Input.GetButtonDown("Menu")) // Menu Button Pressed
+        {
+            if (gamePaused)
+            {
+                ControllerScript.setCursorLocked(true);
+                gamePaused = false;
+            }
+            else
+            {
+                ControllerScript.setCursorLocked(false);
+                gamePaused = true;
+            }
+        }
+    }
+}
