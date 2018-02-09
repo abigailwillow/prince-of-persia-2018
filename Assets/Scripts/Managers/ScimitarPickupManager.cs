2115c488a7c9f82cd04ad517c8b261d452e06a35
Abigail de Joode <abbydiode@gmail.com>
2018-02-09 07:31:15 +0100

    0.0.3
    Added main menu, sound track, scimitar pickup and weapon prototype, audio mixer. WebGL build now online.

---
+++ Assets/Scripts/Managers/ScimitarPickupManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScimitarPickupManager : MonoBehaviour {
+
+    public float VerticalSpeed = 3f;
+    public float RotationSpeed = 50f;
+    public float VerticalRange = 0.1f;
+
+    Vector3 StartPos;
+
+    void Awake()
+    {
+        StartPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, -Time.time * RotationSpeed, 0);
+        transform.position = new Vector3(StartPos.x, StartPos.y + Mathf.Sin(Time.time * VerticalSpeed) * VerticalRange, StartPos.z);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
