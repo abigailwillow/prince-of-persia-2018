98a204952af192cd1a18bff7d55c51427f745edc
Abigail de Joode <abbydiode@gmail.com>
2018-02-28 20:43:56 +0100

    0.0.8
    + Blood Emitter
    + Gates/Buttons
    + Sounds
    + Crouching
    + Pause Menu
    + Viewmodel Swing

---
+++ Assets/Scripts/Logic/PressurePlate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Transform ControllingGate;
+    public Transform PlateTransform;
+    public float ActivatedOffset = 0.1f;
+    public string ClickSound = "Plate_Click";
+
+    GateController Gate;
+    Vector3 Origin;
+    SoundManager SoundOrigin;
+    
+    void Awake()
+    {
+        Gate = ControllingGate.GetComponentInChildren<GateController>();
+        SoundOrigin = GetComponentInChildren<SoundManager>();
+        Origin = PlateTransform.transform.localPosition;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Gate.Open();
+            SoundOrigin.PlaySound(ClickSound);
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            PlateTransform.transform.localPosition = Origin - new Vector3(0, ActivatedOffset, 0);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            PlateTransform.transform.localPosition = Origin;
+            SoundOrigin.PlaySound(ClickSound);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/PauseMenuManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public static bool GamePaused;
+    public GameObject PauseCanvas;
+
+    void Awake()
+    {
+        GamePaused = false;
+        AudioListener.pause = false;
+    }
+
+	void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
+        {
+            if (!GamePaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        AudioListener.pause = false;
+        FPSController.SetCursorLocked(true);
+        GamePaused = false;
+        Time.timeScale = 1f;
+        PauseCanvas.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+        print("Shutdown pending...");
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        AudioListener.pause = true;
+        GamePaused = true;
+        FPSController.SetCursorLocked(false);
+        Time.timeScale = 0f;
+        PauseCanvas.SetActive(true);
+    }
+}
